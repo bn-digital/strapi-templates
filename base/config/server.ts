a76493d6b75b53960dc21deba414928545c2efdb
Alec Vishmidt <alec@bndigital.co>
2024-11-15 19:01:00 +0200

    added server envs

--- base/config/server.ts
+++ base/config/server.ts
@@ -1,7 +1,16 @@
+import { randomSecret } from "../src/hooks"
+import tasks from "../src/functions/cron"
+
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '127.0.0.1'),
+  port: env('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [randomSecret("APP_KEYS")]),
   },
+  cron: {
+    enabled: Object.entries(tasks).length > 0,
+    tasks,
+  },
+  url: env('ABSOLUTE_URL', 'http://localhost:1337'),
+  proxy: true,
 });
